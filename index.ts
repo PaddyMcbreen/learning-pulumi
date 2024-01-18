8fe65557c8f819d84edb5cacf23265b8f2c89ec5
PaddyMcbreen <patrickmcbreen2004@hotmail.com>
2024-01-18 11:27:28 +0000

    VPC IaC created

--- index.ts
+++ index.ts
@@ -2,8 +2,42 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const config = new pulumi.Config()
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+interface vpc {
+    vpc_name: string;
+    vpc_cidr: string;
+    azs: string[];
+    pub_sub_cidrs: string[];
+    priv_sub_cidrs: string[];
+}
+
+
+interface yourDetails {
+    yourIP: string;
+    yourAccessKey: string;
+}
+
+
+
+
+// define objects from config file
+const vpc = config.requireObject<vpc>("vpc")
+
+
+// ----NETWORKING----
+// create VPC
+const main = new aws.ec2.Vpc("main-vpc", {
+    cidrBlock: vpc.vpc_cidr,
+    enableDnsHostnames: true,
+    enableDnsSupport: true,
+    tags: {
+        Name: `${pulumi.getProject()}-${vpc.vpc_name}`,
+        ManagedBy: "Pulumi"
+    }
+})
+
+
+
+// exports:
+export const vpc_export = main
